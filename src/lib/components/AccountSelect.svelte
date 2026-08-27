02f889e8c7fe6fdc1bc71c79985ae5240ffb1876
naggi.dev <namqh17.dev@gmail.com>
2026-08-27 14:21:51 +0900

    feat(#385): 1-to-1 migration of stylesheets, bilingual i18n system, and core shared components

---
+++ src/lib/components/AccountSelect.svelte
@@ -0,0 +1,37 @@
+<ul class="page-subtitle nav">
+  {#each fields as field, index}
+  <li class="nav-item dropdown pe-2">
+    <button type="button" class="btn btn-outline-primary dropdown-toggle"
+      id="field_{index}"
+      rolw="button"
+      data-bs-toggle="dropdown" aria-expanded="false">
+      <BilingualText key={field.titleKey} inline={true} />
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="field_{index}">
+      {#each field.accounts as account}
+      <li>
+        <button type="button" class="dropdown-item btn btn-link"
+          on:click={() => {
+            select(account.code);
+          }}><BilingualText primary={account.name} secondary={account.nameVi} inline={true} /></button>
+      </li>
+      {/each}
+    </ul>
+  </li>
+  {/each}
+</ul>
+<script>
+import {createEventDispatcher} from 'svelte';
+import BilingualText from '$lib/components/BilingualText.svelte';
+
+const dispatch = createEventDispatcher();
+
+export let fields;
+
+const select = (code) => {
+  dispatch('select', {
+    code: code
+  });
+}
+
+</script>
