d1f5587b5b34c7e8964436cc942b785de8665ff6
naggi.dev <namqh17.dev@gmail.com>
2026-08-27 13:12:38 +0900

    chore(#376): remove legacy Express routes, Routify frontend, bin scripts and obsolete config files

--- front/javascripts/logon.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap';
-
-import Logon from '../svelte/logon.svelte';
-
-const index = new Logon({
-    target: document.getElementById('index'),
-});
-
-export default index;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-  import '../../front/stylesheets/style.css';
+  import '../app.css';
 </script>
 
 <slot />
